16e779cfe2ebd4d69e673f65344ed4775be65853
Camilo Patino <jcamilop96@gmail.com>
2021-04-21 17:28:51 -0500

    Add posts model and relations between users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4…10 }
   validates :password, presence: true, length: { in: 4…20 }
 end
