237dc7d5c4013675088aebb025d60b18341dda10
Camilo Patino <jcamilop96@gmail.com>
2021-04-22 15:27:57 -0500

    Add comments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4…10 }
   validates :password, presence: true, length: { in: 4…20 }
 end
