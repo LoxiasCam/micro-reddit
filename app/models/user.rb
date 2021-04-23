0dea01389801fcd74ad60f09e5ee0bc0869df8da
Camilo Patino <jcamilop96@gmail.com>
2021-04-23 15:27:03 -0500

    Refactor User model and remove extranous files and code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4…10 }
-  validates :password, presence: true, length: { in: 4…20 }
+  validates :username, presence: true
+  validates :password, presence: true
 end
